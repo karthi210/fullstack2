1969a7f8c5148e7cff1be15cef323d1832c2b452
karthi210 <119928109+karthi210@users.noreply.github.com>
2026-04-28 16:53:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "mysql-db",
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root123",
   database: process.env.DB_NAME || "contactsdb"
